aff24b80c175ef2e6d25848b3507ed7d6070aae1
lucky favour <luckyfavour1219@gmail.com>
2024-07-26 00:20:22 +0100

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import Veggie from "../Components/Veggie";
 import Popular from "../Components/Popular";
+import { motion } from "framer-motion"; 
 
 
 const Home = () => {
   return (
-    <div>
+    <motion.div animate={{opacity: 1}} initial={{opacity: 0}} exit={{ opacity: 0}} transition={{ duration: 0.5}}>
         <Veggie />
         <Popular />
-    </div>
+    </motion.div>
   )
 }
 
